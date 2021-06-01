380ae89261a951237f21251b92265f4bd4b887b1
Michal Kortas <michal@webroad.pl>
2021-06-01 09:25:54 +0200

    Add file component view

---
+++ src/resources/views/components/form-file.blade.php
@@ -0,0 +1,22 @@
+<div class="form-group {{$groupClass ?? ''}}">
+    @isset($label)
+        <label @isset($id) for="{{$id}}" @endisset @isset($labelClass) class="{{$labelClass}}" @endisset>{{$label ?? ''}}</label>
+    @endisset
+
+    <input
+        type="file"
+        @isset($id) id="{{$id}}" @endisset
+        @isset($name) name="{{$name}}" @endisset
+        class="form-control @isset($class) {{$class}} @endisset @isset($name) @error($name) is-invalid @enderror @endisset"
+        placeholder="{{$placeholder ?? $label ?? ''}}"
+        {{$attributes}}
+    >
+
+        @isset($name)
+            @error($name)
+                <span class="invalid-feedback {{$feedbackClass}}" role="alert">
+                    <strong>{{ ucfirst($message ?? '') }}</strong>
+                </span>
+            @enderror
+        @endisset
+</div>
